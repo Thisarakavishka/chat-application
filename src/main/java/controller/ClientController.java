f0075ea5da22cffcf92b98c670c4ab36bc88c85a
thisara <thisarakavishka0@gmail.com>
2023-06-27 12:14:09 +0530

    Refactor Server and Client

--- src/main/java/controller/ClientController.java
+++ src/main/java/controller/ClientController.java
@@ -7,6 +7,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -21,18 +26,45 @@ public class ClientController implements Initializable {
     @FXML
     private JFXButton sendBtn;
 
+    Socket socket;
+    DataInputStream dataInputStream;
+    DataOutputStream dataOutputStream;
+    String msg = "";
+
     @FXML
     void sendBtnOnAction(ActionEvent event) {
-
+        try {
+        txtArea.appendText("[CLIENT] - "+txtField.getText()+"\n");
+            dataOutputStream.writeUTF(txtField.getText());
+            txtField.setText("");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
     void txtFieldOnAction(ActionEvent event) {
-
+        sendBtnOnAction(event);
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        new Thread(() -> {
+            try {
+                socket = new Socket("Localhost",3002);
+                txtArea.appendText("[CLIENT] - Client connected to server\n");
 
+                dataOutputStream = new DataOutputStream(socket.getOutputStream());
+                dataInputStream = new DataInputStream(socket.getInputStream());
+
+                while (!msg.equalsIgnoreCase("finish")){
+                    msg = dataInputStream.readUTF();
+                    txtArea.appendText("[SERVER] - "+msg+"\n");
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
 }
--- src/main/java/controller/ServerController.java
+++ src/main/java/controller/ServerController.java
@@ -7,6 +7,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -21,18 +26,49 @@ public class ServerController implements Initializable {
     @FXML
     private JFXButton sendBtn;
 
+    ServerSocket serverSocket;
+    Socket socket;
+    DataInputStream dataInputStream;
+    DataOutputStream dataOutputStream;
+    String msg = "";
+
     @FXML
     void sendBtnOnAction(ActionEvent event) {
-
+        try {
+            txtArea.appendText("[SERVER] - " + txtField.getText() + "\n");
+            dataOutputStream.writeUTF(txtField.getText());
+            txtField.setText("");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
     void txtFieldOnAction(ActionEvent event) {
-
+        sendBtnOnAction(event);
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        new Thread(() -> {
+            try {
+                serverSocket = new ServerSocket(3002);
+                txtArea.appendText("[SERVER] - server is started\n");
 
+                socket = serverSocket.accept();
+                txtArea.appendText("[SERVER] - new client is accepted\n");
+
+                dataInputStream = new DataInputStream(socket.getInputStream());
+                dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+                while (!msg.equalsIgnoreCase("finish")) {
+                    msg = dataInputStream.readUTF();
+                    txtArea.appendText("[CLIENT] - " + msg + "\n");
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
 }
